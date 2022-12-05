5dfa8d787cad00f3671a0a58ed098611a7f8f6c7
George Toufenkjian <47486187+GeorgeT-T@users.noreply.github.com>
2022-12-05 13:27:00 -0500

    Added function 2

--- static/script.js
+++ static/script.js
@@ -1,24 +1,53 @@
 document.getElementById('get-cost').addEventListener('click', getTotalCost);
+document.getElementById('get-stock').addEventListener('click', getStocks);
+
 
 function getTotalCost() {
-  const totalCost = document.getElementById('find-cost').value;
-  if (totalCost.value != "") {
-    fetch(`/api/get-total-cost/${totalCost}`)
+  const recipeID = document.getElementById('find-cost').value;
+  if (recipeID.value != "") {
+    fetch(`/api/get-total-cost/${recipeID}`)
       .then(res => res.json()
         .then(data => {
-          if (!totalCost.match(/[a-z]/i) && totalCost.match(/[0-9]/i)) {
+          if (!recipeID.match(/[a-z]/i) && recipeID.match(/[0-9]/i)) {
             const l = document.getElementById('cost-data');
             l.innerHTML = "";
-            totalCost.value = "";
+            recipeID.value = "";
             const item = document.createElement('li');
             console.log(data[0]["Total Cost"])
             item.appendChild(document.createTextNode(data[0]["Total Cost"]))
             l.appendChild(item);
 
           } else {
-            totalCost.value = "";
+            recipeID.value = "";
             alert("PLEASE ENTER NUMBERS ONLY!");
           }
         }))
   }
 }
+
+function getStocks() {
+  const recipeID = document.getElementById('find-stock').value;
+  if (recipeID.value != "") {
+    fetch(`/api/get-stock/${recipeID}`)
+      .then(res => res.json()
+        .then(data => {
+          if (!recipeID.match(/[a-z]/i) && recipeID.match(/[0-9]/i)) {
+            const l = document.getElementById('stock-data');
+            l.innerHTML = "";
+            recipeID.value = "";
+            data.forEach(e => {
+              console.log(e);
+              const item = document.createElement('li');
+              item.appendChild(document.createTextNode("Ingredient Name: " + e.ingredientName + " Ingredient Stock: " + e.ingredientStock + " City: " + e.ingredientName + " Ingredient Amount: " + e.ingredientAmount))
+              l.appendChild(item);
+            })
+
+          } else {
+            recipeID.value = "";
+            alert("PLEASE ENTER NUMBERS ONLY!");
+          }
+        }))
+  }
+}
+
+
