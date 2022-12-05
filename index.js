a2d9ebf4e5486d902924393eb9e6a3c91f4360b0
rivajan <rivajan10@gmail.com>
2022-12-05 00:09:06 -0500

    index page slay add

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
+// var path = require('path');
+// const session = require('express-session');
+app.use(express.static('views'));
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.listen('3000', () => {
     console.log('Server started on port 3000');
@@ -11,14 +17,50 @@ const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'smallworld',
-    database : 'smallworldbakedgoods'
+    database : 'SmallWorld'
 })
 
-// Connect Check
+//Connect Check
 db.connect((err) => {
     if (err){
         throw err;
     }
     console.log('MySQL Connected...')
 });
-np
+
+// app.use(
+//     express.urlencoded({
+//       extended: true
+//     })
+// );
+
+// app.use(session({
+//     secret: "random-value-123",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
+// app.use(express.json());
+
+// //index / homepage 
+// app.get('/', (req, res) => {
+//     //this is our index page....
+//     res.render('index');
+// });
+
+// /*
+// *********************
+//     Recipe Query
+// *********************
+// */
+
+// //register for new members- redirected from index
+// app.get('/recipe', (req, res)=>{
+//     //console.log(req.body); returns nothing bc its a get
+//     res.render('recipe');
+// });
+
+// //handle when they enter the recipe ID and name .
+// app.post('/recipe', (req, res)=>{
+//     // fetch all the data in variable
+//     conn().connect();
